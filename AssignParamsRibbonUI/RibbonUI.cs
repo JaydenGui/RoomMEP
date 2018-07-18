c8159a21fe9d60a9c23ac6bd1b27cf6c7fae81fd
Stanislav <Stanislav@DESKTOP-C6V3BEI>
2018-07-18 21:36:30 +0300

    up

--- AssignParamsRibbonUI/RibbonUI.cs
+++ AssignParamsRibbonUI/RibbonUI.cs
@@ -46,13 +46,13 @@ namespace AssignParamsRibbonUI
             button1.ToolTip = appDesc1;
 
             var appDesc2 = "MEP Cut";
-            var newPanel = newRibbon.AddPanel(app, tabName, appDesc2);
+            var newPanel2 = newRibbon.AddPanel(app, tabName, appDesc2);
             var folderPath2 = @"C:\Program Files\BIMTech Utils\MEP\MEP Room";
             var appNamespace2 = "CutMEPCurvesByMassEdges";
             var dllPath2 = $@"Contents\{appNamespace2}.dll";
             var fullPath2 = Path.Combine(folderPath2, dllPath2);
             //добавляем кнопку на панель
-            var button2 = newPanel.AddButton(
+            var button2 = newPanel2.AddButton(
                 appDesc2,
                 fullPath2,
                 $"{appNamespace2}.Main");
@@ -62,6 +62,25 @@ namespace AssignParamsRibbonUI
             button2.LargeImage = new BitmapImage(new Uri(imageName2, UriKind.Absolute));
             button2.ToolTipImage = new BitmapImage(new Uri(imageName2, UriKind.Absolute));
             button2.ToolTip = appDesc2;
+
+            var appDesc3 = "MEP Rise";
+            var newPanel3 = newRibbon.AddPanel(app, tabName, appDesc3);
+            var folderPath3 = @"C:\Program Files\BIMTech Utils\MEP\MEP Room";
+            var appNamespace3 = "AssignPipeRiseParameter";
+            var dllPath3 = $@"Contents\{appNamespace3}.dll";
+            var fullPath3 = Path.Combine(folderPath3, dllPath3);
+            //добавляем кнопку на панель
+            var button3 = newPanel3.AddButton(
+                appDesc3,
+                fullPath3,
+                $"{appNamespace3}.Main");
+            //задаём кнопке изображение
+            var imageName3 = Path.Combine(folderPath3, @"UI\Images\piperise.png");
+            button3.Image = new BitmapImage(new Uri(imageName3, UriKind.Absolute));
+            button3.LargeImage = new BitmapImage(new Uri(imageName3, UriKind.Absolute));
+            button3.ToolTipImage = new BitmapImage(new Uri(imageName3, UriKind.Absolute));
+            button3.ToolTip = appDesc3;
+
             #endregion
             return Result.Succeeded;
         }
